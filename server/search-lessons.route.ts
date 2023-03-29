466f1c4407e0cdd64d46bea4eadb8e2401be9fbb
Shubham Soni <ssoni6359@gmail.com>
2023-03-29 22:15:47 +0530

    Initial Setup and Run

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -1,22 +1,14 @@
-
-
-
-import {Request, Response} from 'express';
-import {LESSONS} from "./db-data";
-import {setTimeout} from "timers";
-
-
+import { Request, Response } from 'express';
+import { LESSONS } from "./db-data";
+import { setTimeout } from "timers";
 
 export function searchLessons(req: Request, res: Response) {
-
     const queryParams = req.query as any;
-
-    const courseId = queryParams.courseId,
-          filter = queryParams.filter || '',
-          sortOrder = queryParams.sortOrder,
-          pageNumber = parseInt(queryParams.pageNumber) || 0,
-          pageSize = parseInt(queryParams.pageSize);
-
+    const courseId = queryParams.courseId;
+    const filter = queryParams.filter || '';
+    const sortOrder = queryParams.sortOrder;
+    const pageNumber = parseInt(queryParams.pageNumber) || 0;
+    const pageSize = parseInt(queryParams.pageSize);
     let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
 
     if (filter) {
@@ -28,12 +20,8 @@ export function searchLessons(req: Request, res: Response) {
     }
 
     const initialPos = pageNumber * pageSize;
-
     const lessonsPage = lessons.slice(initialPos, initialPos + pageSize);
-
     setTimeout(() => {
         res.status(200).json({payload: lessonsPage});
     },1000);
-
-
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -3,7 +3,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Course} from "../model/course";
 import {map} from "rxjs/operators";
 import {Lesson} from "../model/lesson";
 import {Course} from '../model/course';
