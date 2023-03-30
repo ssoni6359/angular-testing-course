9d1d374210861bdcb1858a6f3cb0b2562f65598d
Shubham Soni <ssoni6359@gmail.com>
2023-03-30 12:53:48 +0530

    lesson 3: Mocking a service

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,13 +3,27 @@ import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
     it('should add two numbers', ()=> {
-        const calculator = new CalculatorService(new LoggerService());
+
+        /* Actual Implemetation of Dependent Service
+         *   const logger = new LoggerService();
+         *   spyOn(logger, 'log');
+        */
+
+        //Fake Implemetaion of Logger Service
+        const logger = jasmine.createSpyObj('LoggerService', ["log"]);
+
+        // logger.log.and.returnValue();
+
+        const calculator = new CalculatorService(logger);
         const result = calculator.add(2,2);
+
         expect(result).toBe(4);
+        expect(logger.log).toHaveBeenCalledTimes(1);
     })
+
     it('should substract two numbers', ()=> {
         const calculator = new CalculatorService(new LoggerService());
         const result = calculator.subtract(2,2);
-        expect(result).toBe(1, "unexpected substraction result");
+        expect(result).toBe(0, "unexpected substraction result");
     })
 })
