55e71411ed6696284d349f88237c90366c01f52c
Shubham Soni <ssoni6359@gmail.com>
2023-03-30 11:31:37 +0530

    lesson 2 - instantiate service and expect method

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,8 +1,15 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
 describe('CalculatorService', () => {
     it('should add two numbers', ()=> {
-        pending();
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.add(2,2);
+        expect(result).toBe(4);
     })
     it('should substract two numbers', ()=> {
-        fail();
+        const calculator = new CalculatorService(new LoggerService());
+        const result = calculator.subtract(2,2);
+        expect(result).toBe(1, "unexpected substraction result");
     })
 })
