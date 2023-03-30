12fb6d82a89f9c418353d82291360a64ede6d0d4
Shubham Soni <ssoni6359@gmail.com>
2023-03-30 16:33:45 +0530

    lesson 5: TestBed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,31 +1,27 @@
 import { CalculatorService } from "./calculator.service";
+import { TestBed } from "@angular/core/testing"
 import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
     let calculator: CalculatorService, loggerSpy: any;
     beforeEach(() => {
         loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
-        calculator = new CalculatorService(loggerSpy);
-
-        console.log("Calling beforeEach");
+        TestBed.configureTestingModule({
+            providers: [ 
+                CalculatorService, 
+                { provide: LoggerService, useValue: loggerSpy}
+            ]
+        })
+        calculator = TestBed.get(CalculatorService);
     })
 
     it('should add two numbers', ()=> {
-        // const logger = jasmine.createSpyObj('LoggerService', ["log"]);
-        // const calculator = new CalculatorService(logger);
-        console.log("Add Test");
-
         const result = calculator.add(2,2);
         expect(result).toBe(4);
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     })
 
     it('should substract two numbers', ()=> {
-        // const logger = jasmine.createSpyObj('LoggerService', ["log"]);
-        // const calculator = new CalculatorService(logger);
-        console.log("Substract Test");
-
-
         const result = calculator.subtract(2,2);
         expect(result).toBe(0, "unexpected substraction result");
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
