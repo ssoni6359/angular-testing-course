2e0a876a37f6bdb09452f61aa557d69030e71780
Shubham Soni <ssoni6359@gmail.com>
2023-03-30 16:16:12 +0530

    lesson 4: beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,28 +2,32 @@ import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
+    let calculator: CalculatorService, loggerSpy: any;
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+        calculator = new CalculatorService(loggerSpy);
+
+        console.log("Calling beforeEach");
+    })
+
     it('should add two numbers', ()=> {
+        // const logger = jasmine.createSpyObj('LoggerService', ["log"]);
+        // const calculator = new CalculatorService(logger);
+        console.log("Add Test");
 
-        /* Actual Implemetation of Dependent Service
-         *   const logger = new LoggerService();
-         *   spyOn(logger, 'log');
-        */
-
-        //Fake Implemetaion of Logger Service
-        const logger = jasmine.createSpyObj('LoggerService', ["log"]);
-
-        // logger.log.and.returnValue();
-
-        const calculator = new CalculatorService(logger);
         const result = calculator.add(2,2);
-
         expect(result).toBe(4);
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     })
 
     it('should substract two numbers', ()=> {
-        const calculator = new CalculatorService(new LoggerService());
+        // const logger = jasmine.createSpyObj('LoggerService', ["log"]);
+        // const calculator = new CalculatorService(logger);
+        console.log("Substract Test");
+
+
         const result = calculator.subtract(2,2);
         expect(result).toBe(0, "unexpected substraction result");
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     })
 })
