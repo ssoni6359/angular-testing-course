658c06b546eadc9bef86f0a99edc567d22785970
Shubham Soni <ssoni6359@gmail.com>
2023-03-30 17:57:21 +0530

    lesson 2: data retrieval http mock

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from "@angular/core/testing";
 import { CoursesService } from "./courses.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
+import { COURSES } from "../../../../server/db-data";
 
 describe('CoursesService', () => {
     let coursesService: CoursesService;
@@ -18,6 +19,17 @@ describe('CoursesService', () => {
     })
 
     it('should retreive all courses', ()=> {
+        coursesService.findAllCourses()
+            .subscribe(courses => {
+                expect(courses).toBeTruthy('No courses returned');
+                expect(courses.length).toBe(12, "incorrect number of courses");
 
+                const course = courses.find(course => course.id == 12);
+                expect(course.titles.description).toBe("Angular Testing Course");
+            });
+
+        const req = httpTestingController.expectOne('/api/courses');
+        expect(req.request.method).toEqual("GET");
+        req.flush({payload: Object.values(COURSES)});
     })
 })
