3f772d81a5eccc8e2603ef74347ea05b868c8482
Shubham Soni <ssoni6359@gmail.com>
2023-03-31 15:01:45 +0530

    lesson 6: combining setTimeout and promise

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -5,7 +5,7 @@ fdescribe('Async Testing Examples', () => {
     it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
         let test = false;
         setTimeout(() => {
-            console.log('running assertions');
+            // console.log('running assertions');
             test = true;
             expect(test).toBeTruthy();
             done();
@@ -17,31 +17,49 @@ fdescribe('Async Testing Examples', () => {
         setTimeout(() => { });
         setTimeout(() => {
             test = true;
-            console.log('running assertions setTimeout()');
+            // console.log('running assertions setTimeout()');
         }, 1000);
         // tick(1000);        
         flush();
         expect(test).toBeTruthy();
     }));
 
-    fit('Asynchronous test example - plain Promise', fakeAsync(() => {
+    it('Asynchronous test example - plain Promise', fakeAsync(() => {
         let test = false;
-        console.log('Creating promise');
+        // console.log('Creating promise');
 
         // setTimeout(()=>{ console.log('setTimeout() first callback triggered'); });
         // setTimeout(()=>{ console.log('setTimeout() second callback triggered'); });
 
         Promise.resolve().then(() => {
-            console.log('Promise first then() resolved successfully');
+            // console.log('Promise first then() resolved successfully');
             // test = true;
             return Promise.resolve();
         }).then(()=> {
-            console.log('Promise second then() resolved successfully');
+            // console.log('Promise second then() resolved successfully');
             test = true;
         });
 
         flushMicrotasks();
-        console.log('running test assertions');
+        // console.log('running test assertions');
         expect(test).toBeTruthy();
+    }));
+
+    it('Asynchronous test example - Promises + setTimeout()', fakeAsync(() => {
+        let counter = 0;
+        Promise.resolve().then(()=> {
+            counter += 10;
+            setTimeout(()=> { counter += 1 }, 1000)
+        });
+        expect(counter).toBe(0);
+        flushMicrotasks();
+        expect(counter).toBe(10);
+
+        tick(500);
+        expect(counter).toBe(10);
+        tick(500);
+        
+        // flush();
+        expect(counter).toBe(11);
     }));
 });
