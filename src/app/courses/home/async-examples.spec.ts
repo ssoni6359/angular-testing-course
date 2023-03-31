f0c97bf912a7fb8e63fe18b14133ecb6ed4653be
Shubham Soni <ssoni6359@gmail.com>
2023-03-31 14:42:25 +0530

    lesson 4: promise and timeout, event queues, micro & macro tasks

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -12,25 +12,44 @@ fdescribe('Async Testing Examples', () => {
         }, 1000);
     });
 
-    //Angular Zone
     it('Asynchronous test example - setTimeout()', fakeAsync(() => {
         let test = false;
-        
-        // setTimeout(() => { });
-
+        setTimeout(() => { });
         setTimeout(() => {
             test = true;
             console.log('running assertions setTimeout()');
-            // expect(test).toBeTruthy(); -> remove assertion from timeout and do as normal
         }, 1000);
-        
-        // tick(1000);
-        // tick(500);
-        // tick(499);
-        // tick(1);
-        
+        // tick(1000);        
         flush();
         expect(test).toBeTruthy();
-
     }));
+
+    //Promise-> micro tasks, setTimeout-> Macro task/ Task
+    fit('Asynchronous test example - plain Promise', () => {
+        let test = false;
+        console.log('Creating promise');
+
+        setTimeout(()=>{ console.log('setTimeout() first callback triggered'); });
+        setTimeout(()=>{ console.log('setTimeout() second callback triggered'); });
+
+        Promise.resolve().then(() => {
+            console.log('Promise first then() resolved successfully');
+            // test = true;
+            return Promise.resolve();
+        }).then(()=> {
+            console.log('Promise second then() resolved successfully');
+            test = true;
+        });
+
+        console.log('running test assertions');
+        expect(test).toBeTruthy();
+    });
 });
+
+// Promise has priority over setTimeout - [Wrong to say]. Instead, first micro task then macro tasks.
+
+// All those asynchronous macro operations(timeout, ajax, clicks, etc.) will get added to the event loop.
+// And between each of these macro tasks, the browser rendering engine is going to get a chance to update.
+
+// Promises are added to their own queue. Micro tasks are more lightweight & allow our runtime to be more responsive.
+//Therefore, there are two separate queues in the JavaScript runtime for asynchronous tasks.
