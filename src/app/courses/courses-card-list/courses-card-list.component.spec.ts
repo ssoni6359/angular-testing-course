3cb4db9a27dd8f9d39f03c1064324b5f229924a0
Shubham Soni <ssoni6359@gmail.com>
2023-03-31 11:07:05 +0530

    lesson 3: more on query

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -30,7 +30,6 @@ describe('CoursesCardListComponent', () => {
     it("should display the course list", () => {
         component.courses = setupCourses();
         fixture.detectChanges();
-        // console.log(el.nativeElement.outerHTML);
 
         const cards = el.queryAll(By.css(".course-card"));
         expect(cards).toBeTruthy('Could not find cards');
@@ -38,6 +37,16 @@ describe('CoursesCardListComponent', () => {
     });
     
     it("should display the first course", () => {
-        pending();
+        component.courses = setupCourses();
+        fixture.detectChanges();
+
+        const course = component.courses[0];
+        const card = el.query(By.css(".course-card:first-child"));
+        const title = card.query(By.css(".mat-mdc-card-title"));
+        const image = card.query(By.css("img"));
+
+        expect(card).toBeTruthy('Could not find course card');
+        expect(title.nativeElement.textContent).toBe(course.titles.description);
+        expect(image.nativeElement.src).toBe(course.iconUrl);
     });
 });
