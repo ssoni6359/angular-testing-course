669334c10ebc69d8fb3b3c35e9e4f4aadfb1dcc0
Shubham Soni <ssoni6359@gmail.com>
2023-03-31 08:45:25 +0530

    lesson 4: Error handling behaviour

--- src/app/courses/services/courses.services.spec.ts
+++ src/app/courses/services/courses.services.spec.ts
@@ -3,6 +3,7 @@ import { CoursesService } from "./courses.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
 import { COURSES } from "../../../../server/db-data";
 import { Course } from "../model/course";
+import { HttpErrorResponse } from "@angular/common/http";
 
 describe('CoursesService', () => {
     let coursesService: CoursesService;
@@ -57,6 +58,24 @@ describe('CoursesService', () => {
         expect(req.request.method).toEqual("PUT");
         expect(req.request.body.titles.description).toBe(changes.titles.description);
         req.flush({ ...COURSES[12], ...changes});
+    });
+
+    it('should give an error if save course fails', () => {
+        const changes: Partial<Course> = { titles: { description: 'Testing Course' }};
+        coursesService.saveCourse(12, changes)
+            .subscribe(
+                ()=> fail('The save course operation should have failed'),
+                (error: HttpErrorResponse) => {
+                    expect(error.status).toBe(500);
+                } 
+            );
+
+        const req = httpTestingController.expectOne('/api/courses/12');
+        expect(req.request.method).toEqual("PUT");
+        req.flush(
+            'Save course failed', 
+            { status: 500, statusText: 'Internal Server Error'}
+        );
     });
 
     afterEach(() => {
