c3b3f8a516ff9f1ac61c2e867a74b34711fb587e
Shubham Soni <ssoni6359@gmail.com>
2023-03-31 20:46:54 +0530

    lesson 1: initial setup: coverage and build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular Testing Course';
 }
+
+// For code coverage -> use 
+// ng test --watch=false --code-coverage
+
+// install: npm install -g http-server [not necessary]
+// run: http-server -c-1 .
+
+// -c-1 -> disable caching headers
+// .    -> current folder
+
+//For build: we use: ng build --prod [old]
+//ng build --configuration=production, or ng build -c=production [new]
